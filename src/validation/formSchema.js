b4daf64461cc8f56f956a18992bfa06743990a72
Casey Harding <casey.r.harding@gmail.com>
2022-02-23 13:03:39 -0500

    Schema in place

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,25 @@
 // Here goes the schema for the form
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required('Username is required ya chump!')
+    .min(3, 'Username has to be more than three characters ya chump!'),
+  email: yup
+    .string()
+    .email('Must be a valid email address ya chump!')
+    .required('Email is required ya chump'),
+  role: yup
+    .string()
+    .oneOf(['instructor', 'student', 'alumni'], 'Role is required COME ON!!!'),
+  civil: yup
+    .required()
+    .oneOf(['married', 'single'], 'Gotta be either married or single, bud.'),
+  coding: yup.boolean(),
+  reading: yup.boolean(),
+  hiking: yup.boolean()
+})
+
+export default formSchema;
