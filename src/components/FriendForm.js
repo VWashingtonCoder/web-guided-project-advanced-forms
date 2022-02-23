dcaa1d683db32960edac94f40c1a02d404fc20e1
Casey Harding <casey.r.harding@gmail.com>
2022-02-23 12:46:52 -0500

    Form in place.

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -21,8 +21,9 @@ export default function FriendForm(props) {
 
   const onChange = evt => {
     /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-    const { name, value } = evt.target
-    change(name, value)
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
   }
 
   return (
